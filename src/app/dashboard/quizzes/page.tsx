e4af5061f3b27f778bcc8b45a5221eef441ec9fb
torexto <tomasztrela8@gmail.com>
2026-04-01 22:55:22 +0200

    quiz selector

---
+++ src/app/dashboard/quizzes/page.tsx
@@ -0,0 +1,19 @@
+﻿"use client";
+
+import QuizSelector from "@/components/dashboard/QuizSelector";
+
+export default function QuizzesPage() {
+   return (
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+         {[1, 2, 3, 4].map((i) => (
+            <QuizSelector
+               key={i}
+               title={i.toString()}
+               description={i.toString()}
+               isActive={i % 2 === 0}
+               openAction={() => {}}
+            />
+         ))}
+      </div>
+   );
+}
